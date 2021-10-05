1b8c3e3c83703e25a5cf31ce7c2e7a36e41baa1f
Junbeom Park <junbum0674@gmail.com>
2021-10-05 19:33:59 +0900

    21.10.05 Spring Di basic setter

---
+++ src/di/basic/setter/InsaImpl.java
@@ -0,0 +1,35 @@
+package di.basic.setter;
+public class InsaImpl implements Insa {
+	MemberDAO dao = new MemberDAO();//null - 참조변수를 멤버변수로 선언하면 초기값은 null
+	public InsaImpl() {
+		System.out.println("InsaImpl의 기본생성자");
+	}
+	
+	public InsaImpl(MemberDAO dao) {
+		super();
+		this.dao = dao;
+		System.out.println("InsaImpl의 매개변수 1개 생성자");
+	}
+
+	public MemberDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(MemberDAO dao) {
+		System.out.println("setDAO호출");
+		this.dao = dao;
+	}
+
+	@Override
+	public void addUser(MemberDTO user) {
+		dao.add(user);
+		
+	}
+
+	@Override
+	public MemberDTO getUser(String id) {
+		dao.getUser("id");
+		return null;
+	}
+
+}
