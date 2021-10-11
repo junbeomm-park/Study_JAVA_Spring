72cb0c9fb5f47cc3ddd9a3b6770fa541f7dfcd8a
Junbeom Park <junbum0674@gmail.com>
2021-10-11 21:04:13 +0900

    21.10.11 Index Controller
    게시판 카테고리 목록 조회

--- src/main/java/main/IndexController.java
+++ src/main/java/main/IndexController.java
@@ -1,13 +1,26 @@
 package main;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import multi.erp.board.BoardService;
+import multi.erp.board.BoardVO;
 
 @Controller
 public class IndexController {
+	@Autowired
+	BoardService service;
 	@RequestMapping("/index.do")
-	public String index() {
-		return "index"; //tiles설정 파일에 등록된 view의 이름
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("index");
+		//index뷰에서 게시판 카테고리의 목록을 조회해야 하므로 
+		List<BoardVO> boardlist = service.findByCategory("게시판");
+		mav.addObject("boardlist", boardlist);
+		return mav; 
 	}
 	
 //	@RequestMapping("/emp/login.do")
